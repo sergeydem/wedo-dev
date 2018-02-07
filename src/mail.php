ad005de8af78a3595407f9200b9aae9d48c14067
sergeydem <s.demjanenko@artorg.com.ua>
2018-02-07 16:02:04 +0200

    настроил отправку в телеграмм сообщений

--- src/mail.php
+++ src/mail.php
@@ -18,13 +18,14 @@ if((isset($_POST['Username'])&&$_POST['Username']!="")&&(isset($_POST['Phone'])&
     $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
     $send = mail($to, $subject, $message, $headers);
 
-//    $message = '<b>Имя: </b>'.$_POST['Username'] . "\n";
-//    $message .= '<b>Фамилия: </b>'.$_POST['Phone'] . "\n";
-//    $message .= '<b>Вопрос: </b>'.$_POST['select'] . "\n";
-//
-//    require 'Telegram.php';
-//    $tg = new Telegram();
-//    $tg->sendMessage($message);
+    $message = '<b>Интернет магазины </b>'. "\n";
+    $message .= '<b>Имя: </b>'.$_POST['Username'] . "\n";
+    $message .= '<b>Фамилия: </b>'.$_POST['Phone'] . "\n";
+    $message .= '<b>Комментарий: </b>'.$_POST['comment'] . "\n";
+
+    require 'Telegram.php';
+    $tg = new Telegram();
+    $tg->sendMessage($message);
 }
 
 if((isset($_POST['Username-com'])&&$_POST['Username-com']!="")&&(isset($_POST['Phone-com'])&&$_POST['Phone-com']!="")){//Проверка отправилось ли наше поля name и не пустые ли они
@@ -47,12 +48,16 @@ if((isset($_POST['Username-com'])&&$_POST['Username-com']!="")&&(isset($_POST['P
     $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
     $send = mail($to, $subject, $message, $headers);
 
-//    $message = '<b>Имя: </b>'.$_POST['Username'] . "\n";
-//    $message .= '<b>Фамилия: </b>'.$_POST['Phone'] . "\n";
-//    $message .= '<b>Вопрос: </b>'.$_POST['select'] . "\n";
-//
-//    require 'Telegram.php';
-//    $tg = new Telegram();
-//    $tg->sendMessage($message);
+    $message = '<b>Коммерческое предложение </b>'. "\n";
+    $message .= '<b>Имя: </b>'.$_POST['Username-com'] . "\n";
+    $message .= '<b>Фамилия: </b>'.$_POST['Phone-com'] . "\n";
+    $message .= '<b>Тариф: </b>'.$_POST['tariff'] . "\n";
+    $message .= '<b>Опции: </b>'.$_POST['options'] . "\n";
+    $message .= '<b>Стоимость: </b>'.$_POST['cost-val'] . "\n";
+    $message .= '<b>Сроки: </b>'.$_POST['date'] . "\n";
+
+    require 'Telegram.php';
+    $tg = new Telegram();
+    $tg->sendMessage($message);
 }
 ?>
